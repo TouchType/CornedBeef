a1d12fe4cb5814a6aea599c23be1cfcf26544dd6
Olli Jones <ojj11@users.noreply.github.com>
2016-06-06 19:34:59 +0100

    Update PunchHoleCoachMark.java

--- cornedbeef/src/main/java/com/swiftkey/cornedbeef/PunchHoleCoachMark.java
+++ cornedbeef/src/main/java/com/swiftkey/cornedbeef/PunchHoleCoachMark.java
@@ -9,7 +9,6 @@ import android.content.res.Configuration;
 import android.os.Build;
 import android.support.annotation.IntDef;
 import android.support.annotation.LayoutRes;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
